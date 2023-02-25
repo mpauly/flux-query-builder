2b5907fefff90ca1112a65dda0c59c4b75f83ee1
Martin Pauly <martinpauly@googlemail.com>
2023-02-25 18:10:50 +0100

    Improve packaging

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export { from } from './base/query';
+export { from, FluxQuery } from './base/query';
+export { FluxFunction } from './base/function';
+export { InfluxRepository } from './influx/repository';
